34a76079a38d447292d3704c5c8b54caab71af77
Simon McWhinnie <simon.mcwhinnie@gmail.com>
2016-01-30 21:41:45 +0000

    update shortcode output to deal with long anime titles

--- includes/class-hummingbird-shortcode.php
+++ includes/class-hummingbird-shortcode.php
@@ -26,7 +26,7 @@ class Hummingbird_Shortcode {
 			}
 		}
 		$options_name = 'hummingbird_option_name';
-		$anime_format = '<div class="anime"><div class = "overlay fade"><p class="noselect">Status: %s</p><p class="noselect">Episodes watched: %d</p><p class="noselect">Episode Count: %d</p><button data-toggle="modal" data-target="#%s" class="btn">Details</button></div><img class="anime-cover" src="%s"><h3 class = "anime-title">%s</h3></div>';
+		$anime_format = '<div class="anime"><div class = "overlay fade"><h5 class="overlay-anime-title">%s</h5><p class="noselect">Status: %s</p><p class="noselect">Episodes watched: %d</p><p class="noselect">Episode Count: %d</p><button data-toggle="modal" data-target="#%s" class="btn">Details</button></div><img class="anime-cover" src="%s"><div class = "anime-title-wrap"><h3 class = "anime-title">%s</h3></div></div>';
 		$lightbox_format = '<div class="modal fade" id="%s"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h3 class="modal-title">%s</h3></div><div class="modal-body"><img class="anime-cover" src="%s"><div class="modal-subcontent"><h4>Synopsis</h4><p>%s</p><h4>Genres</h4><p>%s</p></div></div><div class="modal-footer"><button type="button" class="close btn" data-dismiss="modal" aria-label="Close">Close</button></div></div></div></div>';
 		$feed = get_transient( $transient_id );
 		if(!$feed){
@@ -41,7 +41,7 @@ class Hummingbird_Shortcode {
 		$rendered_shortcode = '<div class = "hummingbird_shortcode">';
 		foreach($json_feed as $library_entry){
 			$anime = $library_entry->anime;
-			$rendered_shortcode .= sprintf( $anime_format, $anime->status, $library_entry->episodes_watched, $anime->episode_count, $anime->slug, $anime->cover_image, $anime->title );
+			$rendered_shortcode .= sprintf( $anime_format, $anime->title, $anime->status, $library_entry->episodes_watched, $anime->episode_count, $anime->slug, $anime->cover_image, $anime->title );
 			$genre_list = "";
 			foreach($anime->genres as $genre){
 				$genre_list .= $genre->name . ", ";
